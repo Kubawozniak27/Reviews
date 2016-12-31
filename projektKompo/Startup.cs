95e5fa3074796939a8354093ac520b1274b2d915
Kuba <kubawozniak27@gmail.com>
2016-12-31 11:32:20 +0100

    start

---
+++ projektKompo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(projektKompo.Startup))]
+namespace projektKompo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
