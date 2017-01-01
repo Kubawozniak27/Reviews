39fec549d48d90f8517c83815876aea76dd336f7
Kuba <kubawozniak27@gmail.com>
2017-01-01 21:46:05 +0100

    Dodanie atrybuty Display

--- projektKompo/Models/Review.cs
+++ projektKompo/Models/Review.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,9 +8,14 @@ namespace projektKompo.Models
 {
     public class Review : IEnitity
     {
+        [Display(Name ="Tytuł")]
         public string Tittle { get; set; }
+
+        [Display(Name ="Opis recenzji")]
         public string Description { get; set; }
+
         public int CategoryId { get; set; }
+
         public virtual ICollection<RattingReview> RattingReview { get; set; }
     }
 }
