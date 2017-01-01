8141ebab0a232bc7dffc9808d89e3c4c1a7d7620
Kuba <kubawozniak27@gmail.com>
2017-01-01 23:08:41 +0100

    dodanie atrybytów do modelu

--- projektKompo/Models/RattingReview.cs
+++ projektKompo/Models/RattingReview.cs
@@ -9,7 +9,10 @@ namespace projektKompo.Models
     public class RattingReview : IEnitity
     {
         [Range(0, 10)]
+        [Display(Name ="Ocena")]
         public int Ratting { get; set; }
+
+        [Display(Name ="Komentarz")]
         public string Comment { get; set; }
         public int ReviewId { get; set; }
         public string Author { get; set; }
