ad5745e3b5e421a499c03c1107f0e832c0ed0a81
Kuba <kubawozniak27@gmail.com>
2017-01-01 19:21:03 +0100

    Dodanie CreateReviewViewModels

---
+++ projektKompo/ViewModels/CreateReviewViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace projektKompo.ViewModels
+{
+    public class CreateReviewViewModel
+    {
+        public string Tittle { get; set; }
+        public string Description { get; set; }
+        public int CategoryId { get; set; }
+        public IEnumerable<SelectListItem> Categories { get; set; }
+    }
+}
